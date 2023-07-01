f24b6659a2b120f4164e194fc59da52ed77bc3ea
enzoclock <enzo.testa@oclock.io>
2023-07-01 11:08:23 +0200

    improve frontend

--- public/index.js
+++ public/index.js
@@ -8,19 +8,18 @@ generatorForm.addEventListener("submit", async (event) => {
   const secondPlayerName = form.get("second-player-name") || "Player 2";
   const firstPlayerLevel = parseInt(form.get("first-player-level"));
   const secondPlayerLevel = parseInt(form.get("second-player-level"));
+  const nbOfPointsToPlay = parseInt(form.get("nb-of-points"));
 
   const bestPlayerCode = firstPlayerLevel > secondPlayerLevel ? 0 : 1;
   const weakPlayerCode = Math.abs(bestPlayerCode - 1);
   const levelDifference = Math.abs(firstPlayerLevel - secondPlayerLevel);
-  const sequence = Array(150).fill().map(generateLikelyPoint);
-
-  console.log(sequence);
+  const sequence = Array(nbOfPointsToPlay).fill().map(generateLikelyPoint);
   
   // Display sequence
   const sequenceResult = document.getElementById("sequence-result");
   sequenceResult.innerHTML = sequence.map(buildPlayerPoint).join("");
 
-  const httpResponse = await fetch("http://localhost:3000/scores", {
+  const httpResponse = await fetch("/scores", {
     method: "POST",
     body: JSON.stringify(sequence),
     headers: { "Content-Type": "application/json" }
