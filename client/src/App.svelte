6b474964a1405cc859daf1a9b7fd0f31231c1554
enzoclock <enzo.testa@oclock.io>
2023-07-01 12:23:32 +0200

    layout

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,65 +1,51 @@
+<Layout>
+  <h2>Settings</h2>
+
+  <form id="generator">
+    <div class="grid">
+      <div>
+        <label for="first-player-name">First player</label>
+        <input type="text" id="first-player-name" name="first-player-name" placeholder="Player 1">
+    
+        <label for="first-player-level">Level</label>
+        <input type="range" id="first-player-level" min="1" max="10" value="5" name="first-player-level">
+      </div>
+  
+      <div>
+        <label for="second-player-name">Second player</label>
+        <input type="text" id="second-player-name" name="second-player-name" placeholder="Player 2">
+    
+        <label for="second-player-level">Level</label>
+        <input type="range" id="second-player-level" min="1" max="10" value="5" name="second-player-level">
+      </div>
+    </div>
+    
+    <div class="grid">
+      <div>
+        <label for="nb-of-points">Number of points to play</label>
+        <input type="number" name="nb-of-points" id="nb-of-points" value="150">
+      </div>
+
+      <button>Generate score sequence</button>
+    </div>
+  </form>
+
+  <h2>Results</h2>
+
+  <article id="sequence-result" class="sequence-result">
+  </article>
+
+  <article id="score-result">
+    <pre><code>
+    </code></pre>
+  </article>
+</Layout>
+
 <script lang="ts">
+  import Layout from "./Layout.svelte";
 </script>
 
-<div class="container">
-  <header>
-    <h1>🎾 Tennis Score Generator 🎾</h1>
-  </header>
-  
-  <main>
-    <h2>Settings</h2>
-  
-    <form id="generator">
-      <div class="grid">
-        <div>
-          <label for="first-player-name">First player</label>
-          <input type="text" id="first-player-name" name="first-player-name" placeholder="Player 1">
-      
-          <label for="first-player-level">Level</label>
-          <input type="range" id="first-player-level" min="1" max="10" value="5" name="first-player-level">
-        </div>
-    
-        <div>
-          <label for="second-player-name">Second player</label>
-          <input type="text" id="second-player-name" name="second-player-name" placeholder="Player 2">
-      
-          <label for="second-player-level">Level</label>
-          <input type="range" id="second-player-level" min="1" max="10" value="5" name="second-player-level">
-        </div>
-      </div>
-      
-      <div class="grid">
-        <div>
-          <label for="nb-of-points">Number of points to play</label>
-          <input type="number" name="nb-of-points" id="nb-of-points" value="150">
-        </div>
-  
-        <button>Generate score sequence</button>
-      </div>
-    </form>
-  
-    <h2>Results</h2>
-  
-    <article id="sequence-result" class="sequence-result">
-    </article>
-  
-    <article id="score-result">
-      <pre><code>
-      </code></pre>
-    </article>
-  
-  </main>
-  
-  <footer>
-    <small>Made with ❤️ by <a href="https://github.com/Shrakka">Enzo</a></small>
-  </footer>
-</div>
-
 <style>
-  header {
-    margin-top: 3rem;
-  }
-
   .sequence-result {
     height: 190px;
     overflow: scroll;
---
+++ client/src/Layout.svelte
@@ -0,0 +1,24 @@
+<div class="container">
+  <header>
+    <h1>🎾 Tennis Score Generator 🎾</h1>
+  </header>
+  
+  <main>
+    <slot/>
+  </main>
+  
+  <footer>
+    <small>Made with ❤️ by <a href="https://github.com/Shrakka">Enzo</a></small>
+  </footer>
+</div>
+
+<style>
+  header {
+    margin-top: 3rem;
+  }
+
+  footer {
+    margin-bottom: 3rem;
+    text-align: right;
+  }
+</style>
